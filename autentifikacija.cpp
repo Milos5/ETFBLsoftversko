6debf64aa8ccadc47c244e9ec6d57c88d65f3f46
FilipAd <fadamovic@gmail.com>
2017-01-13 21:11:16 +0100

    Formatirano

--- autentifikacija.cpp
+++ autentifikacija.cpp
@@ -10,12 +10,12 @@ bool authentication(char user[],char pass[]){
 	char b[255];
 	if(in){
 		while(!in.eof()){    //prolazi od pocetka do kraja .txt fajla
-		in.getline(a,255,'#'); //u string a se smjesta username (# je delimiter izmedju username-a i passworda)
-		in.getline(b,255,'\n');//u string b se smejsta password
-		if((strcmp(user,a)==0) && (strcmp(pass,b)==0)) //provjera da li argumenti predati funkiciji odgovaraju nekoj od kombinacija zapisanim u fajlu
-		return true;
-	  }
+			in.getline(a,255,'#'); //u string a se smjesta username (# je delimiter izmedju username-a i passworda)
+			in.getline(b,255,'\n');//u string b se smejsta password
+			if((strcmp(user,a)==0) && (strcmp(pass,b)==0)) //provjera da li argumenti predati funkiciji odgovaraju nekoj od kombinacija zapisanim u fajlu
+				return true;
+		}
 		return false;
-    }
+	}
 }
 
