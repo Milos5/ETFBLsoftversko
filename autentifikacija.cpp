3ca803fce5b2dd6a63ec38a0483f804a9b8b5f30
FilipAd <fadamovic@gmail.com>
2017-01-14 18:57:25 +0100

    Preimenovanje pomocnih promjenjivih

--- autentifikacija.cpp
+++ autentifikacija.cpp
@@ -6,16 +6,17 @@
 using namespace std;
 bool authentication(char user[],char pass[]){
 	ifstream in("database.txt"); //.txt fajl u kojem se nalaze imena korisnika i njehove lozinke
-	char a[255];
-	char b[255];
+	char username[255];
+	char password[255];
 	if(in){
 		while(!in.eof()){    //prolazi od pocetka do kraja .txt fajla
-			in.getline(a,255,'#'); //u string a se smjesta username (# je delimiter izmedju username-a i passworda)
-			in.getline(b,255,'\n');//u string b se smejsta password
-			if((strcmp(user,a)==0) && (strcmp(pass,b)==0)) //provjera da li argumenti predati funkiciji odgovaraju nekoj od kombinacija zapisanim u fajlu
-				return true;
-		}
-		return false;
+		in.getline(username,255,'#'); //u string a se smjesta username (# je delimiter izmedju username-a i passworda)
+		in.getline(password,255,'\n');//u string b se smejsta password
+		if((strcmp(user,username)==0) & (strcmp(pass,password)==0)) //provjera da li argumenti predati funkiciji odgovaraju nekoj od kombinacija zapisanim u fajlu
+		return true;
 	}
+		return false;
 }
+
+		}
 
