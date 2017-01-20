cc42e00a957c86d249a3c95caa1235767f0a6a01
BNikola <nikola_blagojevic_bl@yahoo.com>
2017-01-20 18:58:54 +0100

    Izmjena formata

--- UlazakNaAutoput/menuIn.cpp
+++ UlazakNaAutoput/menuIn.cpp
@@ -3,30 +3,30 @@
 
 void create(string nodeId, string workerId) {
 //kreiranje potvrde o ulasku na autoput
-        cout << endl << endl << "\t\tKreiranje potvrde" << endl;
-        cout << "\t\t=================" << endl;
-        cout << "\t\tTrenutno logovani: " << workerId << endl;
-        cout << "\t\tTrenutni cvor: " << nodeId << endl;
+        cout << endl << endl << "\t\t\tKreiranje potvrde" << endl;
+        cout << "\t\t\t=================" << endl;
+        cout << "\t\t\tTrenutno logovani: " << workerId << endl;
+        cout << "\t\t\tTrenutni cvor: " << nodeId << endl;
 //trenutno vrijeme sistema
         time_t rawTime;
         struct tm * timeInfo;
         time (&rawTime);
         timeInfo = localtime (&rawTime);
-        cout << "\t\tTrenutno vrijeme: " << asctime(timeInfo) << endl;
+        cout << "\t\t\tTrenutno vrijeme: " << asctime(timeInfo) << endl;
         if (confirmation(workerId, nodeId)==0)
-            cout<< "\t\tUspjesno je sacuvana potvrda!";
+            cout<< "\t\t\tUspjesno je sacuvana potvrda!";
         else
             cout<< "Greska pri cuvanju potvrde!";
 }
 
 void help() {
 //pomoc za korisnika
-    cout << endl << endl << "\t\tPomoc" << endl << "\t\t=====" << endl;
-    cout <<"\t\tDobrodosli, hvala sto koristite nasu aplikaciju!" << endl;
-    cout <<"\t\tDa biste izdali potvrdu, u meniju unesite 1 i pritisnite enter." << endl;
-    cout <<"\t\tOtvorice vam se meni za unos podataka i kreiranje nove potvrde!" << endl << endl;
-    cout <<"\t\tDa biste se izlogovali iz aplikacije," << endl;
-    cout <<"\t\tu meniju nesite 0 i pritisnite enter.";
+    cout << endl << endl << "\t\t\tPomoc" << endl << "\t\t\t=====" << endl;
+    cout <<"\t\t\tDobrodosli, hvala sto koristite nasu aplikaciju!" << endl;
+    cout <<"\t\t\tDa biste izdali potvrdu, u meniju unesite 1 i pritisnite enter." << endl;
+    cout <<"\t\t\tOtvorice vam se meni za unos podataka i kreiranje nove potvrde!" << endl << endl;
+    cout <<"\t\t\tDa biste se izlogovali iz aplikacije," << endl;
+    cout <<"\t\t\tu meniju nesite 0 i pritisnite enter.";
     cout << endl << endl;
 }
 
@@ -36,15 +36,14 @@ void menu(string nodeId, string workerId) {
 //char se koristi kao sprecavanje pogresnog unosa
     char input[10] = {0};
     int select;    //select za switch
-    cout<<"\n\t\t\t\t> > >S.U.M.A< < <" << endl << endl;
-    cout << "\t\tGLAVNI MENI" << endl;
-    cout << "\t\tUnesite redni broj operacije koju zelite da izvrsite" << endl;
+    cout<<"\t\t\t\t  < < < MENU > > >\n\n\n";
+    cout<<"\t\t\tOdaberite opciju\n\n";
 //petlja menija, omogucava se visestruko pozivanje menija, dok se ne odabere odjava
     while(select){
-        cout << "\t\t1. Kreiranje potvrde o ulasku na autoput" << endl;
-        cout << "\t\t2. Pomoc" << endl;
-        cout << "\t\t0. Odjava" << endl << endl;
-        cout << "\t\tVas izbor: ";
+        cout << "\t\t\t1. Kreiranje potvrde o ulasku na autoput" << endl;
+        cout << "\t\t\t2. Pomoc" << endl;
+        cout << "\t\t\t0. Odjava" << endl << endl;
+        cout << "\t\t Vas izbor: ";
         cin >> input;
 //dodjela vrijednosti select, da bi switch radio kako treba
         if(strcmp(input,"1")==0) select=1;
@@ -55,7 +54,7 @@ void menu(string nodeId, string workerId) {
         switch (select) {
             case 0:
                 select = 0;
-                cout << endl << endl <<"\t\tOdjava sa sistema" << endl;
+                cout << endl << endl <<"\t\t\tOdjava sa sistema" << endl;
                 break;
 
             case 1:
@@ -64,13 +63,13 @@ void menu(string nodeId, string workerId) {
                 break;
 
             case 2:
-                help();
+                help();             //pozivanje f-je help
                 cout << endl << endl;
                 break;
 
             default:
-                cout << endl << endl <<"\t\tIzabrali ste opciju koja nije validna" << endl;
-                cout << "\t\tPritisnite enter da udjete u glavni meni." << endl;
+                cout << endl << endl <<"\t\t\tIzabrali ste opciju koja nije validna" << endl;
+                cout << "\t\t\tPritisnite enter da udjete u glavni meni." << endl;
                 cout << endl << endl;
                 break;
         }
