5cde1d66a9719abf6624fe0124e475a65daa88f1
BNikola <nikola_blagojevic_bl@yahoo.com>
2017-01-20 19:00:00 +0100

    Main

--- UlazakNaAutoput/main.cpp
+++ UlazakNaAutoput/main.cpp
@@ -1,11 +1,34 @@
-#include<iostream>
+
+#include "form.h"
+#include "authentication.h"
+#include "menuIn.h"
 
 using namespace std;
 
-int main(){
-    int a,b;
-    cout << "Unesite dva integera:";
-    cin >> a >> b;
-    cout << "Zbir: "<<a+b;
-    //test
+int main() {
+    string userId = "0";
+    int counter = 1;
+    while(counter){
+        userId = loginform();
+
+        if (userId!="0"){
+            cout << "\n\n\t\tDobrodosli " << userId << "!\n";
+            cout << "\t\tUnesite ulazni cvor: ";
+            string nodeId = "0";
+            cin.get();
+            std::getline(std::cin,nodeId, '\n');
+            if(nodeId=="0" || nodeId=="" || nodeId==" " || nodeId =="\n")
+                do {
+                    cout << "Greska pri unosu ulaznog cvora!\n";
+                    cout << "\t\tUnesite ulazni cvor: ";
+                    std::getline(std::cin,nodeId, '\n');
+                }while(nodeId=="0" || nodeId=="" || nodeId==" " || nodeId =="\n");
+                menu(nodeId, userId);
+        }
+        else {
+            cout<<"\n\n\t\tHvala sto koristite nasu aplikaciju!\n";
+            counter = 0;
+        }
+    }
+    return 0;
 }
